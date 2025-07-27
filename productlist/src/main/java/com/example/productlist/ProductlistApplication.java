8f5b1e213baa10f52e564ec9fbdd54f1ed65d09f
Student Yamini <yaminikasetty2000@gmail.com>
2025-07-27 20:11:41 +0530

    new changes

---
+++ productlist/src/main/java/com/example/productlist/ProductlistApplication.java
@@ -0,0 +1,14 @@
+package com.example.productlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductlistApplication.class, args);
+
+	}
+
+}
